cef1deea33e767dbb93ea447a19d52ee158c3f8e
Tobias <tobiasboliveira@gmail.com>
2024-03-12 14:31:31 -0300

    project in progress

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,19 @@ import express from 'express'
 import cors from 'cors'
 import hhtps from 'https'
 import http from 'http'
+import siteRoutes from './routes/site'
+import { requestIntercepter } from './utils/requestintercepter'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.all('*', requestIntercepter)
+
+// app.use('/', adminRoutes)
+app.use('/', siteRoutes)
 
 const runServer = (port: number, server: http.Server) => {
     server.listen(port, () => {
