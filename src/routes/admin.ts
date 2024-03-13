473edce615e7dbdae5c144e7a419f9d19b1168a0
Tobias <tobiasboliveira@gmail.com>
2024-03-13 15:08:31 -0300

    project in progress

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import * as auth from '../controllers/auth'
+import * as events from '../controllers/events'
 
 const router = Router()
 
 router.get('/ping', auth.validate, (req, res) => res.json({ pong: true, admin: true }))
+
 router.post('/login', auth.login)
+
+router.get('/events', auth.validate, events.getAll)
 
 export default router
