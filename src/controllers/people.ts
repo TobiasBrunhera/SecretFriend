df91e7df80f68e6e7ce643c834cb3c37b1ce12c5
Tobias <tobiasboliveira@gmail.com>
2024-03-26 16:16:38 -0300

    project in progress

--- src/controllers/people.ts
+++ src/controllers/people.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import * as people from '../services/people'
+import { z } from "zod";
 
 export const getAll: RequestHandler = async (req, res) => {
     const { id_event, id_group } = req.params
@@ -24,6 +25,30 @@ export const getPerson: RequestHandler = async (req, res) => {
     })
 
     if (personItem) return res.json({ people: personItem })
+
+    res.json({ error: 'Ocorreu um erro!' })
+}
+
+export const addPerson: RequestHandler = async (req, res) => {
+    const { id_event, id_group } = req.params
+
+    const addPersonSchema = z.object({
+        name: z.string(),
+        cpf: z.string().transform(val => val.replace(/\.|-/gm, ''))
+    })
+
+    const body = addPersonSchema.safeParse(req.body)
+
+    if (!body.success) return res.json({ error: 'Dados inválidos!' })
+
+    const newPerson = await people.add({
+        name: body.data.name,
+        cpf: body.data.cpf,
+        id_event: parseInt(id_event),
+        id_group: parseInt(id_group)
+    })
+
+    if (newPerson) res.status(201).json({ people: newPerson })
 
     res.json({ error: 'Ocorreu um erro!' })
 }
